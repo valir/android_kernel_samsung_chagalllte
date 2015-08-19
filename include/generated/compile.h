a772e29d0f0264589b78d68a44ebff33f0d7540e
Valentin Rusu <kde@rusu.info>
2015-08-19 21:47:00 +0200

    adding samsung original files

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Thu Mar 5 17:28:20 KST 2015"
+#define LINUX_COMPILE_BY "kjheo"
+#define LINUX_COMPILE_HOST "kjheo-Samsung-Desktop-System"
+#define LINUX_COMPILER "gcc version 4.7 (GCC) "
